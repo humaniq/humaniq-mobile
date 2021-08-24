7192af5f51db279cef6bf0167453c6489c02af33
Nikita Volkov <litebackend@gmail.com>
2021-08-24 14:44:37 +0300

    profile

--- screens/profile/ProfileScreenModel.ts
+++ screens/profile/ProfileScreenModel.ts
@@ -1,32 +1,7 @@
-import {makeAutoObservable} from "mobx";
-import {getProfileStore} from "../../store/profile/ProfileStore";
-import {getAuthStore} from "../../store/auth/AuthStore";
-import {RootStore} from "../../store/RootStore";
-
 export class ProfileScreenModel {
     initialized = false;
-    rootStore: RootStore;
-    refreshing = false;
 
-    constructor() {
-        makeAutoObservable(this, {}, { autoBind: true });
-    }
-
-    async init(rootStore) {
-        this.rootStore = rootStore;
-        try {
-            await getProfileStore().load(getAuthStore().token);
-            if (getProfileStore().loaded) {
-                this.initialized = true;
-            }
-        } catch (e) {
-            console.log("INIT ERROR", e);
-        }
-    }
-
-    async onRefresh() {
-        this.refreshing = true;
-        await getProfileStore().load(getAuthStore().token);
-        this.refreshing = false
+    init() {
+        this.initialized = true;
     }
 }
