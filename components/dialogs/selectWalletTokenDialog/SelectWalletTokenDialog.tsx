d166ab5aa958db4a4f2db101c5f7f6c6bc48d889
Egor Mikheev <egor.mikheev@humaniq.com>
2021-11-22 11:32:04 +0300

    feature/CORE-142-remove-touchable-opacity

--- components/dialogs/selectWalletTokenDialog/SelectWalletTokenDialog.tsx
+++ components/dialogs/selectWalletTokenDialog/SelectWalletTokenDialog.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useInstance } from "react-ioc";
 import { observer } from "mobx-react-lite";
 import { FlatList } from "react-native";
-import { Colors, Dialog, Text, TouchableOpacity, View } from "react-native-ui-lib";
+import { Colors, Dialog, Text, View } from "react-native-ui-lib";
 import { SelectWalletTokenViewModel } from "./SelectWalletTokenViewModel";
 import { runInAction } from "mobx";
 import { DialogHeader } from "../dialogHeader/DalogHeader";
@@ -14,6 +14,7 @@ export const SelectWalletTokenDialog = observer(() => {
 
   return <Dialog
       width={ "100%" }
+      height={ 350 }
       visible={ view.display }
       containerStyle={ { backgroundColor: Colors.white, borderTopLeftRadius: 30, borderTopRightRadius: 30 } }
       onDismiss={ () => runInAction(() => {
@@ -21,27 +22,26 @@ export const SelectWalletTokenDialog = observer(() => {
       }) }
       bottom
   >
-    <TouchableOpacity style={ { maxHeight: 350 } } onPress={ (e) => console.log(e) }>
-      <DialogHeader onPressIn={ () => {
-        view.display = false
-      } }/>
-      <View padding-16>
-        <Text textM>{ t("selectWalletTokenDialog.title") }</Text>
-      </View>
-      <FlatList
-          data={ view.options }
-          keyExtractor={ (item) => item.symbol }
-          renderItem={ ({ item, index }) => {
-            return <TokenItem key={ index } symbol={ item.symbol } tokenAddress={ item.tokenAddress }
-                              logo={ item.logo }
-                              name={ item.name }
-                              formatBalance={ item.formatBalance } formatFiatBalance={ item.formatFiatBalance }
-                              index={ index }
-                              onPress={ item.onPress }
-            />
-          }
-          }
-      />
-    </TouchableOpacity>
+
+    <DialogHeader onPressIn={ () => {
+      view.display = false
+    } }/>
+    <View padding-16>
+      <Text textM>{ t("selectWalletTokenDialog.title") }</Text>
+    </View>
+    <FlatList
+        data={ view.options }
+        keyExtractor={ (item) => item.symbol }
+        renderItem={ ({ item, index }) => {
+          return <TokenItem key={ index } symbol={ item.symbol } tokenAddress={ item.tokenAddress }
+                            logo={ item.logo }
+                            name={ item.name }
+                            formatBalance={ item.formatBalance } formatFiatBalance={ item.formatFiatBalance }
+                            index={ index }
+                            onPress={ item.onPress }
+          />
+        }
+        }
+    />
   </Dialog>
 })
