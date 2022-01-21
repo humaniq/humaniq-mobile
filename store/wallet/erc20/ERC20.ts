3717b2eb007776cb3a4fa19faae99f6b33456761
Egor Mikheev <egor.mikheev@humaniq.com>
2022-01-21 12:01:17 +0300

    feature/CORE-176-settings-page-fix-layout

--- store/wallet/erc20/ERC20.ts
+++ store/wallet/erc20/ERC20.ts
@@ -10,67 +10,67 @@ import { ERC20Transaction } from "../transaction/ERC20Transaction";
 
 @model("ERC20")
 export class ERC20 extends Model({
-  walletAddress: p(t.string, ""),
-  initialized: p(t.boolean, false),
-  pending: p(t.boolean, false),
-  tokenAddress: p(t.string, ""),
-  name: p(t.string, ""),
-  symbol: p(t.string, ""),
-  logo: p(t.string, ""),
-  thumbnail: p(t.string, ""),
-  decimals: p(t.string, ""),
-  balance: p(t.string, ""),
-  priceUSD: p(t.string, ""),
-  priceEther: p(t.string, ""),
-  transactions: p(t.objectMap(t.model<ERC20Transaction>(ERC20Transaction)), () => objectMap<ERC20Transaction>())
+    walletAddress: p(t.string, ""),
+    initialized: p(t.boolean, false),
+    pending: p(t.boolean, false),
+    tokenAddress: p(t.string, ""),
+    name: p(t.string, ""),
+    symbol: p(t.string, ""),
+    logo: p(t.string, ""),
+    thumbnail: p(t.string, ""),
+    decimals: p(t.string, ""),
+    balance: p(t.string, ""),
+    priceUSD: p(t.string, ""),
+    priceEther: p(t.string, ""),
+    transactions: p(t.objectMap(t.model<ERC20Transaction>(ERC20Transaction)), () => objectMap<ERC20Transaction>())
 }) {
 
-  @action
-  async init() {
-    try {
-      const address = getDictionary().ethToken.get(this.symbol)?.address
-      if (address) {
-        const result = await getMoralisRequest().get(formatRoute(MORALIS_ROUTES.TOKEN.GET_ERC20_PRICE, { address }), {
-          chain: 'eth'
-        })
-        if (result.ok) {
-          runUnprotected(() => {
-            this.priceUSD = result.data.usdPrice
-            getDictionary().ethTokenCurrentAddress.set(this.tokenAddress, this.symbol)
-          })
+    @action
+    async init() {
+        try {
+            const address = getDictionary().ethToken.get(this.symbol)?.address
+            if (address) {
+                const result = await getMoralisRequest().get(formatRoute(MORALIS_ROUTES.TOKEN.GET_ERC20_PRICE, { address }), {
+                    chain: 'eth'
+                })
+                if (result.ok) {
+                    runUnprotected(() => {
+                        this.priceUSD = result.data.usdPrice
+                        getDictionary().ethTokenCurrentAddress.set(this.tokenAddress, this.symbol)
+                    })
+                }
+            }
+        } catch (e) {
+            console.log("ERROR", e)
+        } finally {
+            runUnprotected(() => {
+                this.initialized = true
+            })
         }
-      }
-    } catch (e) {
-      console.log("ERROR", e)
-    } finally {
-      runUnprotected(() => {
-        this.initialized = true
-      })
     }
-  }
 
-  @computed
-  get transactionsList() {
-    return Object.values<ERC20Transaction>(this.transactions.items).sort((a, b) => b.blockTimestamp - a.blockTimestamp)
-  }
+    @computed
+    get transactionsList() {
+        return Object.values<ERC20Transaction>(this.transactions.items).sort((a, b) => b.blockTimestamp - a.blockTimestamp)
+    }
 
-  @computed
-  get valBalance() {
-    return preciseRound(+formatUnits(this.balance, this.decimals).toString())
-  }
+    @computed
+    get valBalance() {
+        return preciseRound(+formatUnits(this.balance, this.decimals).toString())
+    }
 
-  @computed
-  get formatBalance() {
-    return this.valBalance ? beautifyNumber(this.valBalance) : `--/--`
-  }
+    @computed
+    get formatBalance() {
+        return this.valBalance ? beautifyNumber(this.valBalance) : `--/--`
+    }
 
-  @computed
-  get fiatBalance() {
-    return this.priceUSD ? preciseRound(this.valBalance * this.priceUSD) : null
-  }
+    @computed
+    get fiatBalance() {
+        return this.priceUSD ? preciseRound(this.valBalance * this.priceUSD) : null
+    }
 
-  @computed
-  get formatFiatBalance() {
-    return this.fiatBalance ? `$${ beautifyNumber(+this.fiatBalance) }` : `--/--`
-  }
+    @computed
+    get formatFiatBalance() {
+        return this.fiatBalance ? `$${ beautifyNumber(+this.fiatBalance) }` : `--/--`
+    }
 }
