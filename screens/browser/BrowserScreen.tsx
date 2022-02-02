b18abc18373a771ae9e0bea9f5b8821ceb617b3a
Djamshid Djurayev <djurayev.d@gmail.com>
2022-02-03 00:23:23 +0500

    change bg color in search page

--- screens/browser/BrowserScreen.tsx
+++ screens/browser/BrowserScreen.tsx
@@ -30,7 +30,7 @@ const Browser = observer(() => {
     view.init()
   }, [])
 
-  return <Screen backgroundColor={ Colors.bg } statusBarBg={ getBrowserStore().showTabs ? Colors.white  : Colors.bg }
+  return <Screen backgroundColor={ Colors.white } statusBarBg={ getBrowserStore().showTabs ? Colors.white  : Colors.bg }
                  preset="scroll"
                  style={ { minHeight: "100%" } }
   >
