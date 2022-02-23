3f223e683ec1a86a1fd9a62e9e60674c292eae2f
Egor Mikheev <egor.mikheev@humaniq.com>
2022-02-23 11:18:32 +0300

    feature/CORE-206-fix

--- components/dialogs/sendTransactionDialog/SendTransactionDialog.tsx
+++ components/dialogs/sendTransactionDialog/SendTransactionDialog.tsx
@@ -5,7 +5,7 @@ import { observer } from "mobx-react-lite"
 import { DialogHeader } from "../dialogHeader/DalogHeader"
 import { SendTransactionViewModel } from "./SendTransactionViewModel"
 import { t } from "../../../i18n"
-import { getEVMProvider, getWalletStore } from "../../../App"
+import { getWalletStore } from "../../../App"
 import { HIcon } from "../../icon";
 import { WalletItem } from "../../walletItem/WalletItem";
 import { currencyFormat } from "../../../utils/number";
@@ -25,7 +25,6 @@ export const SendTransactionDialog = observer(() => {
             backgroundColor: Colors.bg,
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
-            maxHeight: 600
         } }
         visible={ view.display }
         bottom
@@ -75,8 +74,7 @@ export const SendTransactionDialog = observer(() => {
                                 </View>
                                 <View right>
                                     <Text robotoM text16>{ view.txHumanReadable.feeFiat }</Text>
-                                    <Text marginT-5
-                                          textGrey>{ `${ view.txHumanReadable.fee } ${ view.token.symbol }` }</Text>
+                                    <Text marginT-5 textGrey>{ `${ view.txHumanReadable.fee } ${ "ETH" }` }</Text>
                                 </View>
                             </View>
                             <View style={ { borderBottomWidth: 1, borderBottomColor: Colors.grey, marginLeft: 15 } }/>
@@ -87,7 +85,7 @@ export const SendTransactionDialog = observer(() => {
                                 <View right>
                                     <Text robotoM text16>{ view.txHumanReadable.totalFiat }</Text>
                                     <Text marginT-5 numberOfLines={ 1 }
-                                          textGrey>{ `${ view.txHumanReadable.total } ${ view.token.symbol }` }</Text>
+                                          textGrey>{ `${ view.txHumanReadable.total }` }</Text>
                                 </View>
                             </View>
                         </Card>
@@ -97,26 +95,37 @@ export const SendTransactionDialog = observer(() => {
                             <ExpandableSection
                                 onPress={ () => setExpanded(!expanded) }
                                 expanded={ expanded }
-                                sectionHeader={ <View row padding-8 spread centerV>
+                                sectionHeader={ <><View row padding-8 spread centerV>
                                     <Text text16 robotoM>{ t("transactionScreen.adjustFee") }</Text>
                                     <Button link style={ { height: 30, width: 30 } }
                                             onPress={ () => {
                                                 setExpanded(!expanded)
                                             } }
+
                                     >
                                         { !expanded &&
                                             <HIcon name={ "down" } width={ 14 } style={ { color: Colors.black } }/> }
                                         { expanded &&
                                             <HIcon name={ "up" } width={ 14 } style={ { color: Colors.black } }/> }
                                     </Button>
-                                </View> }
+                                </View>
+                                { expanded && <View style={ { borderBottomWidth: 1, borderBottomColor: Colors.grey, marginLeft: 8 } }/> }
+                                </> }
                             >
-                                <View>
+                                <View row spread padding-16>
                                     {
                                         view.transactionFeeView.options.map((option, index) => {
-                                            return <Ripple onPress={ () => option.onOptionPress() } key={ index }>
-                                                <View paddingH-16 paddingV-10 row width={ '100%' } spread>
-                                                    <View flex-1>
+                                            return <Ripple onPress={ () => option.onOptionPress() } key={ index }
+                                                           style={{
+                                                               borderWidth: 1,
+                                                               borderRadius: 12,
+                                                               borderColor: Colors.grey,
+                                                               width: '30%',
+                                                               overflow: 'hidden'
+                                                           }}
+                                            >
+                                                <View paddingH-16 paddingV-10 width={ '100%' }>
+                                                    <View centerH>
                                                         <Avatar imageStyle={ {
                                                             height: 24,
                                                             width: 24,
@@ -129,33 +138,10 @@ export const SendTransactionDialog = observer(() => {
                                                         >
                                                             { (option.label as any).icon }
                                                         </Avatar>
-                                                    </View>
-                                                    <View flex-3 paddingL-25 centerV left>
-                                                        <View>
-                                                            <Text black text16
-                                                                  robotoM>{ (option.label as any).name }</Text>
-                                                        </View>
-                                                        <View paddingT-5>
-                                                            <Text text14 robotoR
-                                                                  textGrey>{ `< ${ (option.label as any).time } min` }</Text>
-                                                        </View>
-                                                    </View>
-                                                    <View flex-6 centerV right>
-                                                        <View>
-                                                            <Text black text16
-                                                                  robotoM>{ currencyFormat(`${ (option.label as any).feeFiat }`, getWalletStore().currentFiatCurrency) }</Text>
-                                                        </View>
-                                                        <View paddingT-5>
-                                                            <Text text14 robotoR
-                                                                  textGrey>{ `${ (option.label as any).fee } ETH` }</Text>
-                                                        </View>
+                                                        <Text black text16 center
+                                                              robotoM>{ (option.label as any).name }</Text>
                                                     </View>
                                                 </View>
-                                                <View style={ {
-                                                    borderBottomWidth: 1,
-                                                    borderBottomColor: Colors.grey,
-                                                    marginLeft: 70
-                                                } }/>
                                             </Ripple>
                                         })
                                     }
@@ -169,10 +155,6 @@ export const SendTransactionDialog = observer(() => {
                                 t('sendTransactionDialog.insufficientBalance') }
                             </Text>
                         </View>
-                    }
-                    {
-                        view.pending &&
-                        <View row center paddingV-20><LoaderScreen color={ Colors.grey20 }/></View>
                     }
                     <View row width={ "100%" } center paddingT-20>
                         <Button onPress={ () => {
