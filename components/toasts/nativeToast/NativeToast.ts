e4703c2fdbfa120ac8de0527f159e66a82fa2b4d
Djamshid Djurayev <djurayev.d@gmail.com>
2022-03-27 19:47:57 +0500

    added qr code detection module added native toast component for both platform

---
+++ components/toasts/nativeToast/NativeToast.ts
@@ -0,0 +1,21 @@
+import Toast from 'react-native-simple-toast';
+
+const ToastConstants = {
+    SHORT: Toast.SHORT,
+    LONG: Toast.LONG,
+    TOP: Toast.TOP,
+    BOTTOM: Toast.BOTTOM,
+    CENTER: Toast.CENTER,
+}
+
+/**
+ * Wrapper of native toast component
+ */
+export default {
+    show(message: string, duration: number = ToastConstants.SHORT) {
+        Toast.show(message, duration)
+    },
+    showWithGravity(message: string, duration: number = ToastConstants.SHORT, gravity = ToastConstants.BOTTOM) {
+        Toast.showWithGravity(message, duration, gravity)
+    }
+}
