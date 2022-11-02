1a66ce4ff4bb6d6723a748ff22d11c0d639dcd1f
Djamshid Djurayev <djurayev.d@gmail.com>
2022-11-02 21:26:31 +0500

    babel config refactoring

--- App.tsx
+++ App.tsx
@@ -1,63 +1,58 @@
-// import "./shim"
-// import "@ethersproject/shims";
-import { observer } from "mobx-react-lite";
-import { provider, useInstance } from "react-ioc";
-import React, { useEffect } from "react";
-import { Colors, Text, View } from "react-native-ui-lib";
-import "app/theme/color";
-import "app/theme/typography";
-import { AppService } from "app/services/AppService";
-import { applyTheme } from "app/theme/componentTheme";
-import { StorageService } from "./app/services/StorageService";
-import { WalletConnectService } from "./app/services/WalletConnectService";
-import { useWalletConnect, withWalletConnect } from "@walletconnect/react-native-dapp";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Button } from "react-native";
+import { observer } from "mobx-react-lite"
+import { provider, useInstance } from "react-ioc"
+import React, { useEffect } from "react"
+import { Text, View, Button } from "react-native-ui-lib"
+import "app/theme/color"
+import "app/theme/typography"
+import { AppService } from "app/services/AppService"
+import { applyTheme } from "app/theme/componentTheme"
+import { StorageService } from "app/services/StorageService"
+import { WalletConnectService } from "app/services/WalletConnectService"
+import { useWalletConnect, withWalletConnect } from "@walletconnect/react-native-dapp"
+import AsyncStorage from "@react-native-async-storage/async-storage"
 
-applyTheme();
+applyTheme()
 
-require("react-native-ui-lib/config").setConfig({ appScheme: "default" });
+require("react-native-ui-lib/config").setConfig({ appScheme: "light" })
 
 const AppScreen = observer(() => {
 
-  const app = useInstance(AppService);
-  const wc = useInstance(WalletConnectService);
-  const connector = useWalletConnect();
+  const app = useInstance(AppService)
+  const wc = useInstance(WalletConnectService)
+  const connector = useWalletConnect()
 
   useEffect(() => {
-    app.init();
-  }, []);
+    app.init()
+  }, [])
 
   useEffect(() => {
     wc.init(connector)
   }, [connector])
 
-  return <View flex style={ { minHeight: "100%" } }>
+  return <View flex style={{ minHeight: "100%", backgroundColor: "#fff" }}>
     <View flex center>
-      <Text>{ app.counter }</Text>
-      <Button onPress={ () => {
-        console.log("RRERE");
-        app.increment();
-      } } title={ "Increment" }></Button>
-      { !connector.connected ? <Button title={ "Connect" } onPress={ () => connector.connect() } /> :
-        <Button title={ "Kill session" } onPress={ () => connector.killSession() } /> }
+      <Text>{app.counter}</Text>
+      <Button onPress={() => {
+        console.log("RRERE")
+        app.increment()
+      }} label={"Increment"}/>
+      {!connector.connected ? <Button label={"Connect"} onPress={() => connector.connect()}/> :
+        <Button label={"Kill session"} onPress={() => connector.killSession()}/>}
     </View>
-  </View>;
-});
+  </View>
+})
 
-const AppWithProvider = provider()(AppScreen);
+const AppWithProvider = provider()(AppScreen)
 
 AppWithProvider.register(
   AppService,
   StorageService,
   WalletConnectService
-);
-
-// export const App = AppWithProvider;
+)
 
 export const App = withWalletConnect(AppWithProvider, {
   redirectUrl: "mover://",
   storageOptions: {
-    asyncStorage: AsyncStorage,
-  },
-});
+    asyncStorage: AsyncStorage
+  } as any
+})
