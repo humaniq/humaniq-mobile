93c040f61a063692059d9d1963d9146c09f457a7
Djamshid Djurayev <djurayev.d@gmail.com>
2022-11-07 12:24:45 +0500

    code refactoring

--- index.js
+++ index.js
@@ -4,7 +4,6 @@
 import "node-libs-react-native/globals";
 import { AppRegistry } from "react-native";
 import { App } from "./App";
-import { App2 } from "./App2";
 import { expo } from "./app.json";
 
-AppRegistry.registerComponent(expo.name, () => App2);
+AppRegistry.registerComponent(expo.name, () => App);
