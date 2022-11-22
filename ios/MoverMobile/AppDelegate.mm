8d84270451463550085e1ed01c32a4c660b205c5
Djamshid Djurayev <djurayev.d@gmail.com>
2022-11-22 18:49:19 +0500

    ios configure

--- ios/MoverMobile/AppDelegate.mm
+++ ios/MoverMobile/AppDelegate.mm
@@ -1,3 +1,4 @@
+#import <Firebase.h>
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -31,6 +32,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [FIRApp configure];
   RCTAppSetupPrepareApp(application);
 
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
